e5e0cd6d3a113b1f2c470d1f581b05c62852abc3
vi-ht <huynhthanhvi1102@gmail.com>
2025-09-21 01:31:25 +0700

    21/09/2025 1

--- src/component/homepage/IntroContent.js
+++ src/component/homepage/IntroContent.js
@@ -238,6 +238,6 @@ export default function IntroContent() {
                     </TextTemplate>
                 </Box>
             </Box>
-        </BgImageLayout =>
+        </BgImageLayout>
     );
 }
